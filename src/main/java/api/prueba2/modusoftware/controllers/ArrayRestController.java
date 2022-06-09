77c4cbb4f106e7914168d723f92bdc03839cc6c2
Camilo Rodriguez <crodrigr@gmail.com>
2022-06-09 07:34:51 -0500

    second update

--- src/main/java/api/prueba2/modusoftware/controllers/ArrayRestController.java
+++ src/main/java/api/prueba2/modusoftware/controllers/ArrayRestController.java
@@ -22,11 +22,23 @@ public class ArrayRestController {
 	@Autowired
 	private IArrayService arrayService;
 	
+	/**
+	 * Endpoint que obtiene el array por id
+	 * @param id es el id de array que se va ha obtener. 
+	 * @return retorna el objeto array que trae de la base de datos. 
+	 * */
 	@GetMapping("/array/{id}")
 	public Array show(@PathVariable Long id){
 		return arrayService.findById(id);
 	}
 	
+	/**
+	 * Este metodo calcula la lista de los vasos seleccionados apartir del algoritmo de seleccion
+	 * que usa la lista de primos con el número de interaciones. 
+	 * @param id es el id del array de vasos a probar
+	 * @param interaciones numero de intaraciones que va aplicar sobre el array de primos
+	 * @return la lista de vasos seleccionados.
+	 */
 	@GetMapping("/resultado/{id}/{interaciones}")
 	public ArrayList<Integer> getVasos(@PathVariable Long id, @PathVariable int interaciones){
 		
@@ -35,6 +47,12 @@ public class ArrayRestController {
 		
 	}
 	
+	/**
+	 * Algoritmo que selecciona los vasos a partir de la lista de primos y numero de interacciones
+	 * @param listaA lista de vasos a examinar
+	 * @param interaciones numero de interaciones que se aplica sobre la lista de los primos
+	 * @return lista de vasos seleccionados
+	 */
 	private ArrayList<Integer> getResultado(ArrayList<Integer> listaA, int interaciones){
 		
 		  ArrayList<Integer> resultado=new ArrayList<Integer>();
